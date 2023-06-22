80975a07ce68cee1ec41893df960e0827fc40c7a
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-06-22 15:23:06 +0530

    fixed bugs

--- LavenderHaze/settings.py
+++ LavenderHaze/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['13.53.217.120', 'lavenderhaze.online','localhost']
+ALLOWED_HOSTS = ['13.53.217.120', 'lavenderhaze.online','localhost','127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://lavenderhaze.online']
 CORS_ORIGIN_WHITELIST = ['https://lavenderhaze.online']
 
